abb73e99d4c8c04ea37cc1e5973a6679998c1e4f
Fortune Ochi <fortune.o@ilobby.com>
2024-11-22 18:04:22 -0500

    Add HeroSection component and update landing page layout

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,113 +1,69 @@
-"use client";
-
+import HeroSection from "@/components/landing/HeroSection";
 import Navbar from "@/components/landing/Navbar";
-import Link from "next/link";
+import { Button } from "antd";
 import Image from "next/image";
-import React from "react";
-
-import {Button} from 'antd';
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="relative min-h-screen bg-white px-28">
-        {/* Hero Section */}
-        <section
-          className="relative w-full mx-auto mt-8 sm:mt-12 rounded-xl overflow-hidden shadow-lg bg-white"
-          style={{height: "80vh"}}
-        >
-          {/* Background Image */}
-          <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{
-              backgroundImage: `url('/images/landing/hero_section_bg.jpg')`,
-            }}
-          />
+        <HeroSection />
 
-          {/* Overlay for Contrast */}
-          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-
-          {/* Content */}
-          <div className="relative flex flex-col justify-end items-start pb-10 h-full px-8 sm:px-16 text-white z-10">
-            <h1 className="text-3xl sm:text-5xl font-bold leading-snug">
-              All-in-One Support for <br/> New Canadians
-            </h1>
-            <p className="mt-4 text-lg">
-              Explore tools, resources, and expert guidance to help you succeed
-              in your Canadian journey.
-            </p>
-            <Link
-              href="/login"
-              className="mt-6 bg-red-600 hover:bg-red-500 text-white font-semibold px-10 py-3 shadow-md cursor-pointer"
-            >
-              Get Started
-            </Link>
-          </div>
-        </section>
-
-        {/* Features Section */}
+        {/* Features Section 1*/}
         <section className="mt-12 w-full">
           <div className="text-center">
-            <p className="text-sm md:text-base text-gray-600 mb-5">Key Features</p>
+            <p className="text-sm md:text-base text-gray-600 mb-5">
+              Key Features
+            </p>
             <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-8">
-              Everything You Need for a <span className="block">Smooth Transition</span>
+              Everything You Need for a{" "}
+              <span className="block">Smooth Transition</span>
             </h3>
           </div>
           <div className="flex flex-col md:flex-row justify-between items-stretch gap-4">
-            {/* First Card */}
+            {/* Jobs Card */}
             <div className="bg-gray-50 px-12 md:px-14 w-full md:w-1/2 pt-12 flex flex-col justify-between h-full">
               <div>
                 <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold">
                   Jobs and <span className="block">Careers Advice</span>
                 </h3>
-                <p className="text-sm md:text-base lg:text-lg text-gray-400 mt-4">
-                  Build your Canadian CV, prepare for interviews, <span className="block">
-            and access career advice tailored to the
-          </span>
-                  <span className="block">Canadian job market.</span>
+                <p className="text-sm md:text-base lg:text-lg text-gray-400 mt-4 w-1/2">
+                  Build your Canadian CV, prepare for interviews, and access
+                  career advice tailored to the Canadian job market
                 </p>
-                <Button
-                  type="primary"
-                  ghost
-                  className="mt-5 inline-flex items-center justify-center"
-                >
+                <Button type="default" className="mt-5">
                   Learn More
                 </Button>
               </div>
               <Image
                 src="/images/landing/career_feature.svg"
                 alt="career feature"
-                width={500}
-                height={500}
+                width={10}
+                height={10}
                 className="w-full mt-14"
               />
             </div>
 
-            {/* Second Card */}
+            {/* Real Estate Card */}
             <div className="bg-gray-50 w-full md:w-1/2 py-12 flex flex-col justify-between h-full">
               <Image
                 src="/images/landing/rental_listing_feature.svg"
                 alt="rental listing feature"
-                width={500}
-                height={500}
+                width={10}
+                height={10}
                 className="w-full"
               />
               <div className="px-12 md:px-14">
                 <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold">
-                  Real Estate &<span className="block">Housing</span>
+                  Real Estate<span className="block">& Housing</span>
                 </h3>
-                <p className="text-sm md:text-base lg:text-lg text-gray-400 mt-4">
-                  Explore properties, calculate mortgages, <span className="block">
-            and find the perfect place to call home
-          </span>
-                  <span className="block">in Canada.</span>
+                <p className="text-sm md:text-base lg:text-lg text-gray-400 mt-4 w-1/2">
+                  Explore properties, calculate mortgages, and find the perfect
+                  place to call home in Canada.
                 </p>
-                <Button
-                  type="primary"
-                  ghost
-                  className="mt-5 inline-flex items-center justify-center"
-                >
+
+                <Button type="default" className="mt-5">
                   Learn More
                 </Button>
               </div>
@@ -115,7 +71,91 @@ export default function Home() {
           </div>
         </section>
 
+        {/* Features Section 2*/}
+        <section className="mt-5 w-full">
+          <div className="flex flex-col md:flex-row justify-between items-stretch gap-4 h-full">
+            {/* Finance Card */}
+            <div className="bg-gray-50 w-full md:w-1/2 py-12 flex flex-col justify-between h-full">
+              <Image
+                src="/images/landing/finance_feature.svg"
+                alt="rental listing feature"
+                width={10}
+                height={10}
+                className="w-full pl-12"
+              />
+              <div className="px-12 md:px-14">
+                <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold pt-10">
+                  Finance
+                </h3>
+                <p className="text-sm md:text-base lg:text-lg text-gray-400 mt-4 pr-52">
+                  Learn tips on how to manage your finances, access loans, and
+                  understand the Canadian banking system to help you attain your
+                  financial goals.
+                </p>
+                <Button type="default" className="mt-5">
+                  Learn More
+                </Button>
+              </div>
+            </div>
 
+            {/* Right Column */}
+            <div className="w-full md:w-[65%] flex flex-col gap-4">
+              {/* Immigration News Card */}
+              <div className="bg-gray-50 pt-10 flex flex-col justify-between h-full">
+                <div className="px-12 md:px-14 flex-1">
+                  <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold pt-10">
+                    Immigration News<span className="block">& Pathways</span>
+                  </h3>
+                  <p className="text-sm md:text-base lg:text-lg text-gray-400 mt-4 w-1/2">
+                    Stay updated on the latest immigration policies and discover
+                    the best and comprehensive pathways to a Canadian permanent
+                    residency or citizenship.
+                  </p>
+                  <Button type="default" className="mt-5">
+                    Learn More
+                  </Button>
+                </div>
+
+                <div className="flex-1 relative">
+                  <div className="absolute inset-0 bg-gradient-to-b from-gray-50 to-transparent z-10"></div>
+                  <Image
+                    src="/images/landing/immigration_news_feature.jpg"
+                    alt="immigration news feature"
+                    width={2000}
+                    height={2000}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              </div>
+              {/*Grants Card */}
+              <div className="bg-gray-50 flex flex-col md:flex-row justify-between items-center h-full pb-10 md:pb-0">
+                {/* Image on the Left */}
+                <Image
+                  src="/images/landing/grant_feature.jpg"
+                  alt="business opportunity feature"
+                  height={200}
+                  width={400}
+                  className="flex-1 w-full md:w-1/2 h-auto object-contain"
+                />
+
+                {/* Text Content on the Right */}
+                <div className="flex-1 px-12 md:px-14">
+                  <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold mt-5">
+                    Grants
+                  </h3>
+                  <p className="text-sm md:text-base lg:text-lg text-gray-400 mt-4">
+                    Explore opportunities for starting your own business in
+                    Canada, learn about grants, and find networking events to
+                    help you connect with other entrepreneurs.
+                  </p>
+                  <Button type="default" className="mt-5">
+                    Learn More
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
---
+++ src/components/landing/HeroSection.tsx
@@ -0,0 +1,36 @@
+﻿import Link from "next/link";
+
+export default function HeroSection() {
+  return (
+    <section
+      className="relative w-full mx-auto mt-8 sm:mt-12 rounded-xl overflow-hidden shadow-lg bg-white"
+      style={{ height: "80vh" }}
+    >
+      {/* Background Image */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: `url('/images/landing/hero_section_bg.jpg')`,
+        }}
+      />
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      {/* Content */}
+      <div className="relative flex flex-col justify-end items-start pb-10 h-full px-8 sm:px-16 text-white z-10">
+        <h1 className="text-3xl sm:text-5xl font-bold leading-snug">
+          All-in-One Support for <span className="block">New Canadians</span>
+        </h1>
+        <p className="mt-4 text-lg">
+          Explore tools, resources, and expert guidance to help you succeed in
+          your Canadian journey.
+        </p>
+        <Link
+          href="/login"
+          className="mt-6 bg-red-600 hover:bg-red-500 text-white font-semibold px-10 py-3 shadow-md cursor-pointer"
+        >
+          Get Started
+        </Link>
+      </div>
+    </section>
+  );
+}
