6b52f9876fa8f18bce2680e9fc3b6a8ddbc386c5
Fortune Ochi <fortune.o@ilobby.com>
2025-02-12 11:27:49 -0500

    Add infinite scroll functionality to Career Advisor page and update dependencies

--- src/app/(dashboard)/dashboard/career/career-advisor/page.tsx
+++ src/app/(dashboard)/dashboard/career/career-advisor/page.tsx
@@ -22,6 +22,8 @@ import { HiMiniUsers } from "react-icons/hi2";
 import { LuCalendarClock, LuCalendarDays } from "react-icons/lu";
 import { PiSuitcaseSimpleFill } from "react-icons/pi";
 import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export type MeetingType = {
   date: Dayjs;
@@ -37,12 +39,28 @@ export default function CareerAdvisorPage() {
   const [showMeetingScheduleModal, setShowMeetingScheduleModal] =
     useState(false);
   const [selectedAdvisor, setSelectedAdvisor] = useState<Advisor>();
-  const { get } = useApiClient();
-  const { data, error, isLoading } = useSWR<PaginatedList<Advisor>>(
-    "/api/career-advisors",
-    { fetcher: get }
-  );
   const [messageApi, contextHolder] = message.useMessage();
+
+  const getKey = (
+    pageIndex: number,
+    previousPageData: PaginatedList<Advisor>
+  ) =>
+    previousPageData && !previousPageData.items.length
+      ? null
+      : `/api/career-advisors?pageNumber=${pageIndex + 1}&pageSize=10`;
+
+  const { get } = useApiClient();
+
+  const {
+    data: pages,
+    error,
+    isLoading,
+    size,
+    setSize,
+  } = useSWRInfinite<PaginatedList<Advisor>>(getKey, get);
+  const advisors = pages ? pages.flatMap((page) => page.items) : [];
+  const hasMore =
+    pages && pages.length > 0 ? pages[0].totalPages > pages.length : false;
 
   const confirmMeetingSchedule = (meeting: MeetingType) => {
     console.log(meeting);
@@ -63,69 +81,84 @@ export default function CareerAdvisorPage() {
       label: <span className="pl-3">Career Advisors</span>,
       icon: <HiMiniUsers size={20} className="-mb-5" />,
       children: (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-5 md:gap-6 p-4">
-          {data?.items?.map((advisor) => (
-            <Card
-              key={advisor.id}
-              hoverable={false}
-              className="w-full max-w-[300px] mx-auto flex flex-col"
-              cover={
-                <div className="w-full h-56 flex items-center justify-center bg-gray-100 rounded-t-lg">
-                  <Image
-                    alt={advisor.name}
-                    src={advisor.profilePictureUrl}
-                    className="w-full h-full object-contain"
-                  />
-                </div>
-              }
-            >
-              <Meta
-                title={
-                  <div className="text-center font-semibold">
-                    {advisor.name}
+        <InfiniteScroll
+          dataLength={advisors.length}
+          next={() => setSize(size + 1)}
+          hasMore={hasMore}
+          loader={
+            <div className="text-center p-4">Loading more advisors...</div>
+          }
+          endMessage={
+            <p className="text-center p-4 text-gray-500">
+              {isLoading ? "Loading..." : "No more advisors to show."}
+            </p>
+          }
+          scrollableTarget="scrollableContainer"
+        >
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 gap-4 sm:gap-5 md:gap-6 p-4">
+            {advisors.map((advisor) => (
+              <Card
+                key={advisor.id}
+                hoverable={false}
+                className="w-full max-w-[300px] mx-auto flex flex-col"
+                cover={
+                  <div className="w-full h-56 flex items-center justify-center bg-gray-100 rounded-t-lg">
+                    <Image
+                      alt={advisor.name}
+                      src={advisor.profilePictureUrl}
+                      className="!w-full !h-full !object-contain"
+                    />
                   </div>
                 }
-                description={
-                  <div className="text-center text-gray-600 text-sm">
-                    <Space className="font-bold text-nowrap">
-                      <PiSuitcaseSimpleFill />
-                      <span className="text-sm truncate whitespace-nowrap overflow-hidden">
-                        {advisor.title}
-                      </span>
-                    </Space>
-
-                    <span className="text-yellow-500 flex items-center justify-center gap-1">
-                      <StarFilled />
-                      {(advisor.rating || 5).toFixed(1)}
-                    </span>
-                  </div>
-                }
-              />
-
-              {/* Expertise Tags */}
-              <div className="mt-4 flex flex-wrap justify-center gap-1">
-                {advisor.expertise.map((skill) => (
-                  <Tag key={skill.id} className="inline-flex">
-                    {skill.name}
-                  </Tag>
-                ))}
-              </div>
-
-              <Button
-                color="default"
-                variant="filled"
-                block
-                className="!font-dm_sans !font-semibold mt-5"
-                size="large"
-                icon={<ArrowRightOutlined />}
-                iconPosition="end"
-                onClick={() => handleBookSessionClicked(advisor)}
               >
-                Book Session
-              </Button>
-            </Card>
-          ))}
-        </div>
+                <Meta
+                  title={
+                    <div className="text-center font-semibold">
+                      {advisor.name}
+                    </div>
+                  }
+                  description={
+                    <div className="text-center text-gray-600 text-sm">
+                      <Space className="font-bold text-nowrap">
+                        <PiSuitcaseSimpleFill />
+                        <span className="text-sm truncate whitespace-nowrap overflow-hidden">
+                          {advisor.title}
+                        </span>
+                      </Space>
+
+                      <span className="text-yellow-500 flex items-center justify-center gap-1">
+                        <StarFilled />
+                        {(advisor.rating || 5).toFixed(1)}
+                      </span>
+                    </div>
+                  }
+                />
+
+                {/* Expertise Tags */}
+                <div className="mt-4 flex flex-wrap justify-center gap-1">
+                  {advisor.expertise.map((skill) => (
+                    <Tag key={skill.id} className="inline-flex">
+                      {skill.name}
+                    </Tag>
+                  ))}
+                </div>
+
+                <Button
+                  color="default"
+                  variant="filled"
+                  block
+                  className="!font-dm_sans !font-semibold mt-5"
+                  size="large"
+                  icon={<ArrowRightOutlined />}
+                  iconPosition="end"
+                  onClick={() => handleBookSessionClicked(advisor)}
+                >
+                  Book Session
+                </Button>
+              </Card>
+            ))}
+          </div>
+        </InfiniteScroll>
       ),
     },
     {
