880ea97eac1317e83f5a2f45dd0e2610561c3bad
Asaniyan Ayomide Paul <ayomidepaulasaniyan@gmail.com>
2025-04-19 02:49:01 +0100

    code update

--- src/app/(landing)/grants/page.tsx
+++ src/app/(landing)/grants/page.tsx
@@ -1,7 +1,8 @@
 import HeroSection from "@/components/landing/HeroSection";
-import cheveningSchorlarships from "../../../../public/images/landing/cheveningScholarships.png";
-import irelandSchorlarship from "../../../../public/images/landing/irelandScholarships.png";
 import queenMary from "../../../../public/images/landing/queenMary.png";
+import undergraduateGrant from "../../../../public/images/landing/undergraduateGrant.png";
+import irelandSchorlarship from "../../../../public/images/landing/irelandScholarships.png";
+import cheveningSchorlarships from "../../../../public/images/landing/cheveningScholarships.png";
 import client from "@/tina/__generated__/client";
 import { CategoryBlogList } from "@/components/app/CategoryBlogList";
 import { Blogs } from "@/tina/__generated__/types";
@@ -31,6 +32,46 @@ export default async function GrantsPage() {
             return <></>;
         }
       })}
+
+      <section className="">
+        <h2 className="text-center text-2xl text-black font-semibold my-16 font-lato text-[36px] md:text-[42px] lg:text-[48px] leading-[120%]">
+          Popular Grants
+        </h2>
+
+        <section className="py-[100px] bg-[#FFD7D752] px-5">
+          <section className="w-full max-w-[1400px] gap-16 mx-auto flex flex-col md:flex-row items-center">
+            <section className="w-full overflow-clip max-w-[743px] h-[502px] rounded-xl">
+              <Image
+                width={743}
+                height={502}
+                src={undergraduateGrant}
+                alt="undergraduateGrant"
+                className="rounded-xl"
+              />
+            </section>
+
+            <section className="space-y-8">
+              <button className="bg-[#FCFBE7] py-1.5 px-2.5 rounded-md">
+                {" "}
+                Apply Now!{" "}
+              </button>
+              <div className="space-y-4">
+                <h2 className="text-black font-dm_sans text-[36px] md:text-[42px] lg:text-[48px] leading-[120%]">
+                  {" "}
+                  Undergraduate Grant{" "}
+                </h2>
+                <p className="text-[#808080] font-poppins text-lg">
+                  {" "}
+                  Pursue your academic dreams with financial support tailored
+                  for undergraduate students. Apply for grants designed to cover
+                  tuition fees, books, and living expenses, helping you focus on
+                  what matters most—your education.{" "}
+                </p>
+              </div>
+            </section>
+          </section>
+        </section>
+      </section>
 
       <section className="w-full mx-auto max-w-[1400px] px-5">
         <section className="mt-[100px] mb-10">
